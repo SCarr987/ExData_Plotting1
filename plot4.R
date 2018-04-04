9df8e8c6582eec3a2a03fdf802dab90e850baa84
SCarr987 <36315391+SCarr987@users.noreply.github.com>
2018-04-04 08:21:44 -0700

    Uploaded requested .R and .png files for assignment

---
+++ plot4.R
@@ -0,0 +1,52 @@
+#CH4 W1 Project
+
+# Objective: examine how household energy usage varies over a 2-day period in February, 2007
+# Using dataset from UC Irvine Machine Learning Repository, "Individual household electric power consumption Data Set
+
+# load packages
+library(data.table)
+
+# initial load and directory structure
+# remember to set the working directory if needed
+
+fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+if(!file.exists("./data")){dir.create("./data")}           #create a data subdirectory
+if(!file.exists('./data/data.zip')){                       # put the zip file in the data subdirectory
+    download.file(fileUrl,destfile = './data/data.zip')
+}
+
+unzip('./data/data.zip') # Unzip file "household_power_consumption.txt"
+
+# Extract information from dataset
+
+## fast extraction of the rows, option to return as data frame for compatibility with strptime 
+dataSet <- fread("grep [12]/2/2007 data/household_power_consumption.txt", na.strings="?", data.table = FALSE)
+# unique(dataSet$V1)  # shows that format is in D/M/Y - note 22/2/2007 
+
+# D/M/Y select day 1 or 2
+dataSet <- dataSet[(dataSet$V1=="1/2/2007"|dataSet$V1=="2/2/2007"),]
+
+## add column names to dataSet
+colNames <- readLines("data/household_power_consumption.txt", n=1)   # fast to read only one row with readLines
+names(dataSet) <-  strsplit(colNames, "\\;")[[1]]                    # extract character vector from list
+
+## convert date and time 
+dataSet$Time <- strptime(paste(dataSet$Date,dataSet$Time), "%d/%m/%Y %H:%M:%S")  # adds correct date to time
+dataSet$Date <- as.Date(dataSet$Date,"%d/%m/%Y")
+
+# plot 4
+par(mfcol=c(2,2))
+
+plot(dataSet$Time, dataSet$Global_active_power,type='l',ylab="Global Active Power", xlab="")
+
+plot(dataSet$Time, dataSet$Sub_metering_1,type='l', xlab="",ylab ="Energy sub metering")
+lines(dataSet$Time, dataSet$Sub_metering_2,type='l', col='red')
+lines(dataSet$Time, dataSet$Sub_metering_3,type='l', col="blue")
+legend('topright', c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),lty=c(1,1,1),col=c("black","red","blue"))
+
+plot(dataSet$Time, dataSet$Voltage,type='l', ylab="Voltage",xlab="datetime" )
+
+plot(dataSet$Time, dataSet$Global_reactive_power,type='l', ylab="Global_reactive_power",xlab="datetime" )
+# save to .png
+dev.copy(png, file = "plot4.png")
+dev.off()
